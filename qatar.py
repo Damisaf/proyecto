56489c3ca0252d9ab7b8ffab8a04b155d892dc99
Damisaf <109141323+Damisaf@users.noreply.github.com>
2022-09-05 12:07:44 -0300

    Add files via upload

--- qatar.py
+++ qatar.py
@@ -4,21 +4,6 @@ Proyecto [Python]
 Autor: Damian Safdie
 Version: 1.0
 
-Descripción: Programa que registra apuestas de los resultados del Mundial de Qatar 2022.
-registra los usuarios, permite a cada uno predecir como saldran los partidos y en base a ello
-se genera una tabla de posiciones de los usuarios segun la cantidad de aciertos.
-
-para ello se utilizaran 2 archivos csv
-1) usuarios.csv: tendra el id y los datos del apostador, al final tendra un string de
-64 posiciones donde cada una de sus posiciones corespondera a los pronosticos
-de los 64 partidos del mundial. En cada una de las 64 posiciones podra registrarse
-"L" o "E" o "V", cuando se genera el usuario sera un string de 64 "."
-
-2) partidos.csv: tendra los 64 partidos del mundial con sus datos, en la ultima
-posicion tendra el resultado real del partido. ("L","E" o "V" seran las posibilidades)
-
-ambos archivos tienen mas datos de los que se usan aca porque estan pensados para
-continuar mas adelante.
 
 '''
 
--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
+Proyevto  integrador: Curso Python Inicial
+
+Descripción: Programa que registra apuestas de los resultados del Mundial de Qatar 2022.
+Registra los usuarios, permite a cada uno predecir como saldran los partidos y en base a ello
+se genera una tabla de posiciones de los usuarios segun la cantidad de aciertos.
+
+para ello se utilizaran 2 archivos csv
+1) usuarios.csv: tendra el id y los datos del apostador, al final tendra un string de
+64 posiciones donde cada una de sus posiciones corespondera a los pronosticos
+de los 64 partidos del mundial. En cada una de las 64 posiciones podra registrarse
+"L" o "E" o "V", cuando se genera el usuario sera un string de 64 "."
+
+2) partidos.csv: tendra los 64 partidos del mundial con sus datos, en la ultima
+posicion tendra el resultado real del partido. ("L","E" o "V" seran las posibilidades)
+
+Ambos archivos tienen mas datos de los que se usan aca porque estan pensados para
+continuar mas adelante.
+
 
